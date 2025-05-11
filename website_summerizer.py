08daebf6a4d130916137d78345dfd878dfa879a2
haseebblue <haseebullahbj@gmail.com>
2025-05-12 01:45:53 +0500

    this the web summerizer it will use openai api

---
+++ website_summerizer.py
@@ -0,0 +1,52 @@
+import os
+import requests
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from IPython.display import Markdown, display
+from openai import OpenAI
+
+
+load_dotenv()
+
+api_key = os.getenv('OPENAI_API_KEY')
+
+if api_key:
+    print('api key found!!')
+
+else:
+    print('no api key found!!!')
+
+
+openai = OpenAI()
+
+class Website:
+
+    url:str
+    title:str
+    text:str
+
+    def __init__(self, url):
+
+        self.url = url
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.title = soup.title.string if soup.title else "No title found"
+
+        for irrelevent in soup.body(['script', 'style', 'img', 'input']):
+            irrelevent.decompose()
+
+        self.text =  soup.body.get_text(separator="\n", strip=True)
+
+
+web = Website("https://edwarddonner.com")
+
+print(web.title)
+
+print(web.text)
+
+
+system_prompt = "You are an assistant that analyzes the contents of a website\
+    and provides a short summery, ignoring test that might be navigation related\
+    Respond in markdown."
+
+
